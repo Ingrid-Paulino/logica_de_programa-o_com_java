1ebe40eaa4f9d85fe11df6365f81b3ab3f1afbc5
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-05-30 10:59:42 -0300

    Aula 1: Como o Java funciona

---
+++ Aula 1/Algoritmo.java
@@ -0,0 +1,17 @@
+// 1 - Criar o algoritmo
+  // OBS: O nome da class tem que ser igual o do arquivo
+public class Algoritmo {
+  public static void main(String[] args) {
+    System.out.println("Olá!!");
+  }
+}
+
+// 2 - compilar o algoritmo
+  // javac NomeArquivo.java
+  // javac Algoritmo.java
+  //OBS: O arquivo pre compilado que é criado, nós não mexemos. Esse arquivo que depois sera executado pelo nosso computador.
+
+// 3 - Executar o arquivo compilado
+  // java nomeArquivoCompilado
+  // java Algoritmo
+
