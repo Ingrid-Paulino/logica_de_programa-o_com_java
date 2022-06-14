10bd373fb0ce52e49dc99202a3f3b43dfb26ce29
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-06-14 10:22:28 -0300

    :recycle: refactor: Refatorado minhas pastas do bloco 2

--- Bloco2_variaveis_e_constantes/EXERCICIOS FEITOS/Exercicio3-booleano/PassouDeAno.java
+++ Bloco2_variaveis_e_constantes/EXERCICIOS FEITOS/Exercicio3-booleano/PassouDeAno.java
@@ -4,15 +4,15 @@ public class PassouDeAno {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    System.out.println("Digite sua nota: ");
+    System.out.print("Digite sua nota: ");
     Integer notaEstudante = scanner.nextInt();
 
     Boolean nota = notaEstudante >= 70;
 
     if (nota) {
-      System.out.println("Você passou!");
+      System.out.print("Você passou!");
     } else {
-      System.out.println("Você não passou!");
+      System.out.print("Você não passou!");
     }
 
     scanner.close();
