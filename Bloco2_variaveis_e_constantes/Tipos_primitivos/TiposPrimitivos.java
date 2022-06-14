7632eceb42bd8880807495a4b801f69194e4502b
Ingrid-Paulino <ingridpaulino1012@gmail.com>
2022-06-14 10:01:15 -0300

    :sparkles: feat: Adicionado exemplo da aula de dados de tipo primitivo

---
+++ Bloco2_variaveis_e_constantes/Tipos_primitivos/TiposPrimitivos.java
@@ -0,0 +1,33 @@
+public class TiposPrimitivos {
+  public static void main(String[] args) {
+
+    // Principal Diferença ente tipo normal e primitivo:
+       // 1 - normal: pode estar em um estado de ausencia de valor. EX: Byte variavelByte = null;
+       // 2 - primitivo: não aceita valores nulos
+       // 3 - Não existe primitivo de string
+       
+    // Byte variavelByte = 1; // Tipo normal
+    byte variavelByte = 1;  // Tipo primitivo
+
+    // Short variavelShort = 1;
+    short variavelShort = 1;
+
+    // Integer variavelInterger = 1;
+    int variavelInterger = 1;
+
+    // Long variavelLong = 1L;
+    long variavelLong = 1L;
+
+    // Float variavelFloat = 1.0f;
+    float variavelFloat = 1.0f;
+
+    // Double variavelDouble = 1.0;
+    double variavelDouble = 1.0;
+
+    // Character variavelCharacter = 'A';
+    char variavelCharacter = 'A';
+
+    // Boolean variavelBoolean = true;
+    boolean variavelBoolean = true;
+  }
+}
